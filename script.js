5d7f4e4444c755ac0279b862546cc3e35bace027
Mathieu Plourde <mat.plourde@gmail.com>
2014-05-20 23:47:57 -0400

    Added set bonuses to elemental and elemental elite dps. Refactored stuff to use the raw attributes.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// TODO: INCLUDE SET BONUSES!!
-// TODO: USE RAW ATTRIBUTS IN THE JSON TO MATCH THE AFFIXES
+// TODO: ADD SET BONUSES TO THE DPS!!
 
 var damage = 0;
 var slots = ["head", "torso", "feet", "hands", "shoulders", "legs", "bracers", "mainHand", "offHand", "waist", "rightFinger", "leftFinger", "neck"];
 
+var parsedSlots = 0;
+
+var setNames;
+var setBonuses;
+var setCounts;
+// if the player has a RROG, we increment the set count by 1 if it's at least at 2
+var hasRROG
 var fireDmg;
-var iceDmg;
+var coldDmg;
 var lightDmg;
 var physicalDmg;
 var poisonDmg;
@@ -25,20 +31,80 @@ function extractEliteDmg(attribute)
     return parseInt(dmg.substring(0, dmg.length - 1));
 }
 
-var parsedSlots = 0;
+function updateSetBonuses()
+{
+    for(var i = 0; i < setNames.length; i++)
+	{
+	    if(hasRROG && setCounts[i] > 1)
+		{
+		    setCounts[i] = setCounts[i] + 1;
+		}
+		
+		// parse the ranks
+		for(var j = 0; j < setBonuses[i].length; j++)
+		{
+		    if(setCounts[i] >= setBonuses[i][j].required)
+			{
+			    // we apply the bonus if it's something meaningful
+			    parseAttributes(setBonuses[i][j].attributesRaw);
+			}
+		}
+	}
+}
+
 function updateNumbers()
 {
     parsedSlots++;
     if(parsedSlots == slots.length)
     {
-        var elementalMult = 1 + Math.max(fireDmg, iceDmg, lightDmg, physicalDmg, poisonDmg, holyDmg, arcaneDmg) / 100.0;
+	    updateSetBonuses();
+	
+        var elementalMult = 1 + Math.max(fireDmg, coldDmg, lightDmg, physicalDmg, poisonDmg, holyDmg, arcaneDmg);
 
-        var eliteElementalMult = 1 + eliteDmg / 100.0;
+        var eliteElementalMult = 1 + eliteDmg;
 
         $(".attributes-core.secondary").prepend('<li class="tip"><span class="label">Elemental Elite Damage</span><span class="value">' + Math.round(damage * eliteElementalMult * elementalMult)  + '</span></li>');
         $(".attributes-core.secondary").height(130).prepend('<li class="tip"><span class="label">Elemental Damage</span><span class="value">' + Math.round(damage * elementalMult)  + '</span></li>');
     }
 }
+
+function parseAttributes(attributesRaw)
+{
+    if(attributesRaw[ELEMENT_PREFIX + "Fire"] != null)
+	{
+		fireDmg += attributesRaw[ELEMENT_PREFIX + "Fire"].min;
+	}
+	if(attributesRaw[ELEMENT_PREFIX + "Cold"] != null)
+	{
+		coldDmg += attributesRaw[ELEMENT_PREFIX + "Cold"].min;
+	}
+	if(attributesRaw[ELEMENT_PREFIX + "Lightning"] != null)
+	{
+		lightDmg += attributesRaw[ELEMENT_PREFIX + "Lightning"].min;
+	}
+	if(attributesRaw[ELEMENT_PREFIX + "Physical"] != null)
+	{
+		physicalDmg += attributesRaw[ELEMENT_PREFIX + "Physical"].min;
+	}
+	if(attributesRaw[ELEMENT_PREFIX + "Poison"] != null)
+	{
+		poisonDmg += attributesRaw[ELEMENT_PREFIX + "Poison"].min;
+	}
+	if(attributesRaw[ELEMENT_PREFIX + "Holy"] != null)
+	{
+		holyDmg += attributesRaw[ELEMENT_PREFIX + "Holy"].min;
+	}
+	if(attributesRaw[ELEMENT_PREFIX + "Arcane"] != null)
+	{
+		arcaneDmg += attributesRaw[ELEMENT_PREFIX + "Arcane"].min;
+	}
+	if(attributesRaw["Damage_Percent_Bonus_Vs_Elites"] != null)
+	{
+		eliteDmg += attributesRaw["Damage_Percent_Bonus_Vs_Elites"].min;
+	}
+}
+
+var ELEMENT_PREFIX = "Damage_Dealt_Percent_Bonus#";
 
 function parseSlot(slot)
 {
@@ -49,41 +115,30 @@ function parseSlot(slot)
         var path = slotElt.attr("data-d3tooltip");
 
         $.getJSON( "http://us.battle.net/api/d3/data/" + path, function( data ) {
-            for(var i = 0; i < data.attributes.primary.length; i++)
-            {
-                if(data.attributes.primary[i].text.indexOf("Fire skills deal ") != -1)
-                {
-                    fireDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Cold skills deal ") != -1)
-                {
-                    coldDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Lightning skills deal ") != -1)
-                {
-                    lightDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Physical skills deal ") != -1)
-                {
-                    physicalDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Poison skills deal ") != -1)
-                {
-                    poisonDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Holy skills deal ") != -1)
-                {
-                    holyDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Arcane skills deal ") != -1)
-                {
-                    arcaneDmg += extractDmg(data.attributes.primary[i].text);
-                }
-                if(data.attributes.primary[i].text.indexOf("Increases damage against elites by ") != -1)
-                {
-                    eliteDmg += extractEliteDmg(data.attributes.primary[i].text);
-                }
-            }
+		
+		    if(data.id == "Unique_Ring_107_x1")
+			{
+			    hasRROG = true;
+			}
+		
+		    parseAttributes(data.attributesRaw);
+			
+			// set bonuses
+			if(data.set != null)
+			{
+				// we keep the set in a list with an increment
+				var index = setNames.indexOf(data.set.slug);
+				if(index != -1)
+				{
+					setCounts[index] = setCounts[index] + 1;
+				}
+				else
+				{
+					setNames.push(data.set.slug);
+					setBonuses.push(data.set.ranks);
+					setCounts.push(1);
+				}
+			}
     
             updateNumbers();
         });
@@ -96,16 +151,20 @@ function parseSlot(slot)
 
 function parseSlots()
 {
+    setNames = [];
+	setCounts = [];
+	setBonuses = [];
+	hasRROG = false;
     parsedSlots = 0;
-    fireDmg = 0;
-    iceDmg = 0;
-    lightDmg = 0;
-    physicalDmg = 0;
-    poisonDmg = 0;
-    holyDmg = 0;
-    arcaneDmg = 0;
-
-    eliteDmg = 0;
+    fireDmg = 0.0;
+    coldDmg = 0.0;
+    lightDmg = 0.0;
+    physicalDmg = 0.0;
+    poisonDmg = 0.0;
+    holyDmg = 0.0;
+    arcaneDmg = 0.0;
+	
+    eliteDmg = 0.0;
 
     damage = $(".attributes-core.secondary").children("li:first").children("span:last").text();
 

f27ee2fb8eecb0bece6326c99bf8d83970026786
Mathieu Plourde <mat.plourde@gmail.com>
2014-05-26 21:30:06 -0400

    Better error handling for json requests. Fixed language issue

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// TODO: DOESN'T WORK IN OTHER LANGUAGES THAN ENGLISH (BECAUSE OF THE MAINSTAT THINGY...)
 
 var damage = 0;
 var slots = ["head", "torso", "feet", "hands", "shoulders", "legs", "bracers", "mainHand", "offHand", "waist", "rightFinger", "leftFinger", "neck"];
@@ -32,6 +31,8 @@ var bonusCritDmg;
 var bonusCritChance
 var bonusAtkSpeed;
 var bonusMainStat;
+
+var errorStr;
 
 function extractDmg(attribute)
 {
@@ -71,64 +72,72 @@ function updateNumbers()
     parsedSlots++;
     if(parsedSlots == slots.length)
     {
-        updateSetBonuses();
-    
-        if(weaponCount > 1)
+        if(errorStr != "")
         {
-            // dual wielding gives a 15% atk speed bonus
-            baseAtkSpeed += 0.15;
+            // we show the error link instead of the damage
+            $("<div id='errorDiv' />").appendTo("body").hide().text(errorStr);
+            $(".attributes-core.secondary").prepend('<li class="tip"><span class="label"><a href="#" onclick="alert($(\'#errorDiv\').text());return false;">Error</a></span></li>');
         }
-    
-        // include the set bonuses from crit chance, crit dmg, atk speed and main stat
-        var baseDmg = damage / ((1 + baseCritChance * baseCritDmg) * (1 + baseAtkSpeed) * (1 + baseMainStat / 100.0));
-        var realDmg = baseDmg * (1 + (baseCritChance + bonusCritChance) * (baseCritDmg + bonusCritDmg)) * (1 + baseAtkSpeed+bonusAtkSpeed) * (1 + (baseMainStat + bonusMainStat) / 100.0);
-        var roundedRealDmg = Math.round(realDmg);
+        else
+        {    
+            updateSetBonuses();
         
-        var elementalMult = 1 + Math.max(fireDmg, coldDmg, lightDmg, physicalDmg, poisonDmg, holyDmg, arcaneDmg);
-        var eliteElementalMult = 1 + eliteDmg;
-        
-        var elementalDmg = realDmg * elementalMult;
-        var eliteElementalDmg = realDmg * eliteElementalMult * elementalMult;
-        
-        
-        // elemental tooltip
-        var elementalContentCell = $("#tooltip-dps-elemental");
-        if(elementalContentCell.length == 0)
-        {
-            var elementalTooltip = $("<div/>").addClass("ui-tooltip").appendTo("body").hide();
-            elementalContentCell = $("<div id='tooltip-dps-elemental'/>").addClass("tooltip-content").appendTo(elementalTooltip);
-        }
-        elementalContentCell.html('<div xmlns="http://www.w3.org/1999/xhtml" class="profile-tooltip"><p><span class="d3-color-gold">Elemental damage: <span class="d3-color-green"><span class="value">' + elementalDmg.toFixed(2) + '</span></span></span><br /><span class="tooltip-icon-bullet"></span> The amount of skill damage per second you can deal with your highest element.<br /><span class="tooltip-icon-bullet"></span> Damage is based on your weapons, attributes, attack speed, Critical Hit Chance,Critical Hit Damage, elemental damage increase, set bonuses, passive skills, and dual-wielding attack speed increase.</p></div>');
-        
-        // elemental elite tooltip
-        var eliteContentCell = $("#tooltip-dps-elemental-elite");
-        if(eliteContentCell.length == 0)
-        {
-            var eliteTooltip = $("<div/>").addClass("ui-tooltip").appendTo("body").hide();
-            eliteContentCell = $("<div id='tooltip-dps-elemental-elite'/>").addClass("tooltip-content").appendTo(eliteTooltip);
-        }
-        eliteContentCell.html('<div xmlns="http://www.w3.org/1999/xhtml" class="profile-tooltip"><p><span class="d3-color-gold">Elemental elite damage: <span class="d3-color-green"><span class="value">' + eliteElementalDmg.toFixed(2) + '</span></span></span><br /><span class="tooltip-icon-bullet"></span> The amount of skill damage per second you can deal to elites with your highest element.<br /><span class="tooltip-icon-bullet"></span> Damage is based on your weapons, attributes, attack speed, Critical Hit Chance,Critical Hit Damage, elemental damage increase, set bonuses, passive skills, and dual-wielding attack speed increase.</p></div>');
-        
-        var height = 130;
-        
-        if(roundedRealDmg != Math.round(damage))
-        {
-            // real damage tooltip
-            var realContentCell = $("#tooltip-dps-real");
-            if(realContentCell.length == 0)
+            if(weaponCount > 1)
             {
-                var realTooltip = $("<div/>").addClass("ui-tooltip").appendTo("body").hide();
-                realContentCell = $("<div id='tooltip-dps-real'/>").addClass("tooltip-content").appendTo(realTooltip);
+                // dual wielding gives a 15% atk speed bonus
+                baseAtkSpeed += 0.15;
             }
-            realContentCell.html('<div xmlns="http://www.w3.org/1999/xhtml" class="profile-tooltip"><p><span class="d3-color-gold">Actual damage: <span class="d3-color-green"><span class="value">' + realDmg.toFixed(2) + '</span></span></span><br /><span class="tooltip-icon-bullet"></span> The amount damage per second you can deal, including your set bonuses.<br /><span class="tooltip-icon-bullet"></span> Damage is based on your weapons, attributes, attack speed, Critical Hit Chance,Critical Hit Damage, set bonuses, passive skills, and dual-wielding attack speed increase.</p></div>');
         
-            height += 26;
-        
-            $(".attributes-core.secondary").prepend('<li data-tooltip="#tooltip-dps-real" class="tip"><span class="label">Actual damage</span><span class="value">' + roundedRealDmg  + '</span></li>');
+            // include the set bonuses from crit chance, crit dmg, atk speed and main stat
+            var baseDmg = damage / ((1 + baseCritChance * baseCritDmg) * (1 + baseAtkSpeed) * (1 + baseMainStat / 100.0));
+            var realDmg = baseDmg * (1 + (baseCritChance + bonusCritChance) * (baseCritDmg + bonusCritDmg)) * (1 + baseAtkSpeed+bonusAtkSpeed) * (1 + (baseMainStat + bonusMainStat) / 100.0);
+            var roundedRealDmg = Math.round(realDmg);
+            
+            var elementalMult = 1 + Math.max(fireDmg, coldDmg, lightDmg, physicalDmg, poisonDmg, holyDmg, arcaneDmg);
+            var eliteElementalMult = 1 + eliteDmg;
+            
+            var elementalDmg = realDmg * elementalMult;
+            var eliteElementalDmg = realDmg * eliteElementalMult * elementalMult;
+            
+            
+            // elemental tooltip
+            var elementalContentCell = $("#tooltip-dps-elemental");
+            if(elementalContentCell.length == 0)
+            {
+                var elementalTooltip = $("<div/>").addClass("ui-tooltip").appendTo("body").hide();
+                elementalContentCell = $("<div id='tooltip-dps-elemental'/>").addClass("tooltip-content").appendTo(elementalTooltip);
+            }
+            elementalContentCell.html('<div xmlns="http://www.w3.org/1999/xhtml" class="profile-tooltip"><p><span class="d3-color-gold">Elemental damage: <span class="d3-color-green"><span class="value">' + elementalDmg.toFixed(2) + '</span></span></span><br /><span class="tooltip-icon-bullet"></span> The amount of skill damage per second you can deal with your highest element.<br /><span class="tooltip-icon-bullet"></span> Damage is based on your weapons, attributes, attack speed, Critical Hit Chance,Critical Hit Damage, elemental damage increase, set bonuses, passive skills, and dual-wielding attack speed increase.</p></div>');
+            
+            // elemental elite tooltip
+            var eliteContentCell = $("#tooltip-dps-elemental-elite");
+            if(eliteContentCell.length == 0)
+            {
+                var eliteTooltip = $("<div/>").addClass("ui-tooltip").appendTo("body").hide();
+                eliteContentCell = $("<div id='tooltip-dps-elemental-elite'/>").addClass("tooltip-content").appendTo(eliteTooltip);
+            }
+            eliteContentCell.html('<div xmlns="http://www.w3.org/1999/xhtml" class="profile-tooltip"><p><span class="d3-color-gold">Elemental elite damage: <span class="d3-color-green"><span class="value">' + eliteElementalDmg.toFixed(2) + '</span></span></span><br /><span class="tooltip-icon-bullet"></span> The amount of skill damage per second you can deal to elites with your highest element.<br /><span class="tooltip-icon-bullet"></span> Damage is based on your weapons, attributes, attack speed, Critical Hit Chance,Critical Hit Damage, elemental damage increase, set bonuses, passive skills, and dual-wielding attack speed increase.</p></div>');
+            
+            var height = 130;
+            
+            if(roundedRealDmg != Math.round(damage))
+            {
+                // real damage tooltip
+                var realContentCell = $("#tooltip-dps-real");
+                if(realContentCell.length == 0)
+                {
+                    var realTooltip = $("<div/>").addClass("ui-tooltip").appendTo("body").hide();
+                    realContentCell = $("<div id='tooltip-dps-real'/>").addClass("tooltip-content").appendTo(realTooltip);
+                }
+                realContentCell.html('<div xmlns="http://www.w3.org/1999/xhtml" class="profile-tooltip"><p><span class="d3-color-gold">Actual damage: <span class="d3-color-green"><span class="value">' + realDmg.toFixed(2) + '</span></span></span><br /><span class="tooltip-icon-bullet"></span> The amount damage per second you can deal, including your set bonuses.<br /><span class="tooltip-icon-bullet"></span> Damage is based on your weapons, attributes, attack speed, Critical Hit Chance,Critical Hit Damage, set bonuses, passive skills, and dual-wielding attack speed increase.</p></div>');
+            
+                height += 26;
+            
+                $(".attributes-core.secondary").prepend('<li data-tooltip="#tooltip-dps-real" class="tip"><span class="label">Actual damage</span><span class="value">' + roundedRealDmg  + '</span></li>');
+            }
+            $(".attributes-core.secondary").prepend('<li data-tooltip="#tooltip-dps-elemental-elite" class="tip"><span class="label">Elemental elite damage</span><span class="value">' + Math.round(eliteElementalDmg) + '</span></li>');
+            $(".attributes-core.secondary").height(height).prepend('<li data-tooltip="#tooltip-dps-elemental" class="tip"><span class="label">Elemental damage</span><span class="value">' + Math.round(elementalDmg) + '</span></li>');
         }
-        $(".attributes-core.secondary").prepend('<li data-tooltip="#tooltip-dps-elemental-elite" class="tip"><span class="label">Elemental elite damage</span><span class="value">' + Math.round(eliteElementalDmg) + '</span></li>');
-        $(".attributes-core.secondary").height(height).prepend('<li data-tooltip="#tooltip-dps-elemental" class="tip"><span class="label">Elemental damage</span><span class="value">' + Math.round(elementalDmg) + '</span></li>');
-        
     }
 }
 
@@ -222,7 +231,7 @@ function parseSlot(slot)
     if(slotElt.length != 0)
     {
         var path = slotElt.attr("data-d3tooltip");
-
+        
         $.getJSON( window.location.protocol + "//" + window.location.hostname + "/api/d3/data/" + path, function( data ) {
         
             if(data.id == "Unique_Ring_107_x1")
@@ -265,6 +274,11 @@ function parseSlot(slot)
             }
     
             updateNumbers();
+        }).fail(function(jqxhr, textStatus, error) {
+        
+            errorStr += "Error: " + textStatus + ", " + error + " (" + window.location.protocol + "//" + window.location.hostname + "/api/d3/data/wawa" + path + "). ";
+            
+            updateNumbers();
         });
     }
     else
@@ -291,7 +305,6 @@ function parseSlots()
     
     eliteDmg = 0.0;
     
-    
     baseCritChance = 0.05;
     baseCritDmg = 0.5;
     baseAtkSpeed = 0.0;
@@ -304,18 +317,24 @@ function parseSlots()
     
     damage = $(".attributes-core.secondary").children("li:first").children("span:last").text();
     
-    var characterClass = $("h2.class").text().split(" ").slice(1).join(" ").replace(/(\r\n|\n|\r)/gm,"").trim();
-    if(characterClass.indexOf("Monk") != -1 || characterClass.indexOf("Demon Hunter") != -1)
+    errorStr = "";
+    
+    var nameArray = $("#heroes").parent().attr("class").split("-");
+    var characterClass = nameArray.slice(0, nameArray.length - 1).join("-").replace(/(\r\n|\n|\r)/gm,"").trim();
+    switch(characterClass)
     {
-        mainStat = "Dexterity";
-    }
-    else if(characterClass.indexOf("Witch Doctor") != -1 || characterClass.indexOf("Wizard") != -1)
-    {
-        mainStat = "Intelligence";
-    }
-    else if(characterClass.indexOf("Barbarian") != -1 || characterClass.indexOf("Crusader") != -1)
-    {
-        mainStat = "Strength";
+        case "monk":
+        case "demon-hunter":
+            mainStat = "Dexterity";
+            break;
+        case "witch-doctor":
+        case "wizard":
+            mainStat = "Intelligence";
+            break;
+        case "barbarian":
+        case "crusader":
+            mainStat = "Strength";
+            break;
     }
     
     baseMainStat = parseInt($("li[data-tooltip=#tooltip-" + mainStat.toLowerCase() + "-hero]").find(".value").text());
